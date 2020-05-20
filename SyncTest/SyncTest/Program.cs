491c2f1d7929712f2c8ea58a02df610e3dcfac9d
Ateo13 <xxxAteoxxc@gmail.com>
2020-05-20 17:44:22 +0300

    This is a sync testing

---
+++ SyncTest/SyncTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//Just sync testing
+namespace SyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am testing a synchronization");
+        }
+    }
+}
